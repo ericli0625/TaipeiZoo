173c21c3d3f22d6acdfa381597ba652d7a45d035
ericli0625 <ericli0625@gmail.com>
2021-09-18 02:41:25 +0800

    [feat] improve cache mechanism.

--- app/src/main/java/com/example/taipeizoo/util/Constants.kt
+++ app/src/main/java/com/example/taipeizoo/util/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
     const val HOUSE_ID = "id"
     const val HOUSE_NAME = "houseName"
     const val PLANT_NAME = "plantName"
+
+    const val DATE_PATTERN_SLASH_SEPARATE = "yyyy/MM/dd"
 }
